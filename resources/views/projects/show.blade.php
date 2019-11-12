d7643fc22ba8949c3e9115e8eb9b35b8cee6b9a1
nstolk <nick_stolk9@hotmail.com>
2019-11-12 15:14:39 +0100

    Activity blade pages, Rendering activity with polymorphism

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -62,6 +62,7 @@
 
             <div class="lg:w-1/3 px-3">
                 @include ('projects.card')
+                @include ('projects.activity.card')
             </div>
         </div>
     </main>
