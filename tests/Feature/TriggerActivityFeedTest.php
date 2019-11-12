e4f4c11443eca8bf49d3aefca5fbd818c494007a
nstolk <nick_stolk9@hotmail.com>
2019-11-12 17:51:50 +0100

    MorphMany relationship created activity subject_body and subject_id for completing and incompleting tasks, The subject of the activity

--- tests/Feature/TriggerActivityFeedTest.php
+++ tests/Feature/TriggerActivityFeedTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use phpDocumentor\Reflection\ProjectFactory;
@@ -27,6 +28,7 @@ class TriggerActivityFeedTest extends TestCase
         $project->update(['title' => 'changed']);
 
         $this->assertCount(2, $project->activity);
+
         $this->assertEquals('updated', $project->activity->last()->description);
     }
 
@@ -37,7 +39,13 @@ class TriggerActivityFeedTest extends TestCase
         $project->addTask('Some task');
 
         $this->assertCount(2, $project->activity);
-        $this->assertEquals('created_task', $project->activity->last()->description);
+
+        tap($project->activity->last(), function ($activity) {
+            $this->assertEquals('created_task', $activity->description);
+            $this->assertInstanceOf(Task::class, $activity->subject);
+            $this->assertEquals('Some task', $activity->subject->body);
+        });
+
     }
 
     public function test_completing_a_task()
@@ -54,6 +62,11 @@ class TriggerActivityFeedTest extends TestCase
 
         $this->assertCount(3, $project->activity);
         $this->assertEquals('completed_task', $project->activity->last()->description);
+
+        tap($project->activity->last(), function ($activity) {
+            $this->assertEquals('completed_task', $activity->description);
+            $this->assertInstanceOf(Task::class, $activity->subject);
+        });
     }
 
     public function test_incompleting_a_task()
