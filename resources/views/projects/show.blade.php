c1630147e975c2d4c156367140ef9746d173a025
nstolk <nick_stolk9@hotmail.com>
2019-11-14 16:23:36 +0100

    Easy Tailwind themes using CSS variables

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <header class="flex items-center mb-3 py-4">
         <div class="flex justify-between items-end w-full">
-            <p class="text-grey text-sm font-normal">
-                <a href="/projects" class="text-grey text-sm font-normal no-underline">My Projects</a>
+            <p class="text-default text-sm font-normal">
+                <a href="/projects" class="text-default text-sm font-normal no-underline">My Projects</a>
                 / {{ $project->title }}
             </p>
 
@@ -28,16 +28,16 @@
         <div class="lg:flex -mx-3 mb-6">
             <div class="lg:w-3/4 px-3 mb-6">
                 <div class="mb-8">
-                    <h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
+                    <h2 class="text-lg text-default font-normal mb-3">Tasks</h2>
                     {{-- tasks --}}
                     @foreach ($project->tasks as $task)
                         <div class="card mb-3">
                             <form method="POST" action="{{ $task->path() }}">
                                 @method('PATCH')
                                 @csrf
-                                <div class="flex">
+                                <div class="flex items-center">
                                     <input name="body" value="{{ $task->body }}"
-                                           class="w-full" {{$task->completed ? 'text-grey' : ''}}>
+                                           class="bg-card text-default w-full" {{$task->completed ? 'text-default' : ''}}>
                                     <input name="completed" type="checkbox"
                                            onChange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
                                 </div>
@@ -48,23 +48,19 @@
                     <div class="card mb-3">
                         <form method="POST" action="{{ $project->path() . '/tasks' }}">
                             @csrf
-                            <input type="text" placeholder="Add a new task" class="w-full" name="body">
+                            <input type="text" placeholder="Add a new task..." class="bg-card text-default w-full" name="body">
                         </form>
                     </div>
                 </div>
 
                 <div>
-                    <h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
+                    <h2 class="text-lg text-default font-normal mb-3">General Notes</h2>
 
                     {{-- general notes --}}
                     <form method="POST" action="{{ $project->path() }}">
                         @method('PATCH')
                         @csrf
-                        <textarea name="notes"
-                                  class="card w-full mb-4"
-                                  style="min-height: 200px"
-                                  placeholder="Anything special you want to make a note of?"
-                        >{{ $project->notes }}</textarea>
+                        <textarea name="notes" class="card w-full mb-4" style="min-height: 200px" placeholder="Anything special you want to make a note of?">{{ $project->notes }}</textarea>
 
                         <button type="submit" class="button">Save</button>
                     </form>
